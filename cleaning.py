067492742a424d9f7cf3c51f9dfcbd4ca5739c91
Glenn Dawson <trebledawson@gmail.com>
2017-12-24 20:14:33 -0500

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -2,36 +2,72 @@ import chess
 import numpy as np
 from tools import all_possible_moves
 import pickle
+import time
 
-fp = open('C:\Glenn\Stuff\Machine Learning\chess\Data\GMallboth.pgn')
+fp = open('.\Data\GMallboth.pgn')
 
 game_records = [[], [], []]
 game_length = 0
+white = 0
+black = 0
 poss = all_possible_moves()
+games = 0
+dump_check = 0
+first_check = True
+file_number = 1
+illegal_check = False
+start = time.time()
 for line, data in enumerate(fp):
-    if (line - 11) % 16 == 0:
+    if (line - 11) % 16 == 0:               # Lines containing game records
         game_length = 0
+        states = []
+        pis = []
         board = chess.Bitboard()
         game_records[0] = game_records[0] + [board.fen()]
         print(line + 1, ':', data)
         moves = data.split()
         del moves[::3]
         for move in moves:
-            board.push_san(move)
-            game_records[0] = game_records[0] + [board.fen()]
+            try:
+                # If move is legal, play move, extract FEN, and set pi for
+                # that position to 0.9 for the played move. For all other
+                # legal moves, set the probabilities to 1/(n-1) where n is
+                # the number of legal moves.
+                # If there is only one possible move, set that probability to 1
+                indices = [poss.index(move_.uci()) for move_ in
+                           board.generate_legal_moves()]
 
-            move_ = board.peek().uci()
-            index = poss.index(move_)
-            pi = np.zeros(1968)
-            pi[index] = 1
+                board.push_san(move)
+                states.append(board.fen())
 
-            game_records[1] = game_records[1] + [pi]
+                move_ = board.peek().uci()
+                index = poss.index(move_)
+                pi = np.zeros(1968)
+                if len(indices) > 1:
+                    prob = 0.1 / len(indices)
+                    for idx in indices:
+                        pi[idx] = prob
+                    pi[index] = 0.9
+                else:
+                    pi[index] = 1
 
-            game_length += 1
-        del game_records[0][-1]
+                pis.append(pi)
 
+                game_length += 1
+            except:
+                # If move is illegal, delete current game record and skip
+                # results processing
+                print('Illegal move detected at move', game_length / 2)
+                illegal_check = True
+                break
 
-    elif (line - 13) % 16 == 0:
+    elif (line - 13) % 16 == 0:             # Lines containing results of games analyzed in previous step
+        if illegal_check:
+            # Check if illegal move encountered
+            game_records = [[], [], []]
+            illegal_check = False
+            continue
+
         print(line + 1, ':', data)
         data = data.split()
         if data[0] == '1-0':
@@ -42,34 +78,99 @@ for line, data in enumerate(fp):
             winner = -1
 
         # Assign rewards and penalties
+        del game_records[0][-1]
         if winner == 0:
+            # Write board states and pis to master record
+            game_records[0] = game_records[0] + states
+            game_records[1] = game_records[1] + pis
+
             # Reward Player 1, penalize Player 2
             Z = np.zeros(game_length)
             Z[::2] = 1
             Z[1::2] = -1
-            Z = Z.tolist()
-            game_records[2] = game_records[2] + Z
+            game_records[2] = game_records[2] + Z.tolist()
+
+            # Internal
+            white += 1
         elif winner == 1:
+            # Write board states and pis to master record
+            game_records[0] = game_records[0] + states
+            game_records[1] = game_records[1] + pis
+
             # Penalize Player 1, penalize Player 2
             Z = np.zeros(game_length)
             Z[::2] = -1
             Z[1::2] = 1
-            Z = Z.tolist()
-            game_records[2] = game_records[2] + Z
+            game_records[2] = game_records[2] + Z.tolist()
+
+            # Internal
+            black += 1
         else:
-            # Slightly penalize draws
-            Z = np.full(game_length, -0.25)
-            Z = Z.tolist()
-            game_records[2] = game_records[2] + Z
+            # Throw out draws
+            continue
 
-        print('Positions:', len(game_records[0]))
-        print('Probabilities:', len(game_records[1]))
-        print('Results:', len(game_records[2]))
+        games += 1
+        print('Games:', games, '\n')
 
+        dump_check += 1
+
+        filename = '.\Data\GMdata' + str(file_number) + \
+                   '.pickle'
+
+        if first_check:
+            # If this is the first game in a new set, open a new file
+            picklefile = open(filename, 'wb')
+            pickle.dump(game_records, picklefile)
+            picklefile.close()
+            first_check = False
+
+        if dump_check == 400:
+            # Each 400 games, write to pickle and delete active memory
+            picklefile = open(filename, 'rb')
+            records = pickle.load(picklefile)
+            picklefile.close()
+
+            records[0] = records[0] + game_records[0]
+            records[1] = records[1] + game_records[1]
+            records[2] = records[2] + game_records[2]
+
+            print('Positions:', len(records[0]))
+            print('Probabilities:', len(records[1]))
+            print('Results:', len(records[2]))
+            print('White wins:', white, '| Black wins:', black)
+            print('Elapsed time: ', time.time() - start, 'seconds.\n')
+
+            picklefile = open(filename, 'wb')
+            pickle.dump(records, picklefile)
+            picklefile.close()
+
+            game_records = [[], [], []]
+
+            dump_check = 0
+
+            if len(records[0]) > 20000:
+                # If pickle files exceed 20000 positions, start a new file
+                first_check = True
+                file_number += 1
 fp.close()
 
-fp = open('C:\Glenn\Stuff\Machine Learning\chess\Data\GMdata.pickle', 'wb')
-pickle.dump(game_records, fp)
-fp.close()
+# Write final games to pickle
+filename = '.\Data\GMdata' + str(file_number) + '.pickle'
+picklefile = open(filename, 'rb')
+records = pickle.load(picklefile)
+picklefile.close()
+
+records[0] = records[0] + game_records[0]
+records[1] = records[1] + game_records[1]
+records[2] = records[2] + game_records[2]
+
+print('Positions:', len(records[0]))
+
+picklefile = open(filename, 'wb')
+pickle.dump(records, picklefile)
+picklefile.close()
+
+print('Elapsed time: ', time.time() - start, 'seconds.\n')
+
 
 print('Done.')
